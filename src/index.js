783b8d78e080084460b268dee5e65d89aa9afd1d
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2019-01-09 09:47:45 -0800

    Working on service workers.

--- src/index.js
+++ src/index.js
@@ -9,7 +9,6 @@ ReactDOM.render(<App />, document.getElementById('root'));
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
 // Learn more about service workers: http://bit.ly/CRA-PWA
-// serviceWorker.unregister();
 serviceWorker.register();
 
 //Add to home screen button
@@ -33,9 +32,9 @@ window.addEventListener('beforeinstallprompt', e => {
     // Wait for the user to respond to the prompt
     deferredPrompt.userChoice.then(choiceResult => {
       if (choiceResult.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
+        console.log('User accepted the time-machine prompt');
       } else {
-        console.log('User dismissed the A2HS prompt');
+        console.log('User dismissed the time-machine prompt');
       }
       deferredPrompt = null;
     });
