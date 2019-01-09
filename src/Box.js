cbbbc2a4804c204ba32f70c96aa798edef0e7a48
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2019-01-09 08:45:40 -0800

    Added time travel.

--- src/Box.js
+++ src/Box.js
@@ -1,45 +1,87 @@
 import React, { Component } from 'react';
 import './Box.css';
+import tmSound from './time-machine.mp3';
 
 export default class Box extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      one: '#',
-      two: '#',
-      three: '#',
-      four: '#',
-      five: '#'
+      display: '#####',
+      play: false
     };
     this.handleClick = this.handleClick.bind(this);
   }
 
   handleClick(evt) {
-    console.log(evt.target.textContent);
     let num = evt.target.textContent;
-    if (this.state.one === '#') this.setState({ one: num });
-    else {
-      if (this.state.two === '#') this.setState({ two: num });
-      else {
-        if (this.state.three === '#') this.setState({ three: num });
-        else {
-          if (this.state.four === '#') this.setState({ four: num });
-          else {
-            if (this.state.five === '#') this.setState({ five: num });
-            else {
-              // ADD A PAUSE AFTER 5 and check entry. If not correct maybe flash something or just clear
-              this.setState({
-                one: '#',
-                two: '#',
-                three: '#',
-                four: '#',
-                five: '#'
-              });
-            }
-          }
+
+    for (let i = 0; i < this.state.display.length; i++) {
+      if (this.state.display[i] === '#') {
+        let str =
+          this.state.display.slice(0, i) +
+          num +
+          this.state.display.slice(i + 1);
+
+        if (i === 4) {
+          this.setState(st => {
+            st.display = str;
+            this.checkCode(st.display);
+          });
+        } else {
+          return this.setState({ display: str });
         }
       }
     }
+  }
+
+  checkCode(code) {
+    if (code === '12345') {
+      this.setState({ display: '2019' });
+      setTimeout(() => {
+        this.setState({ display: '2018' });
+        setTimeout(() => {
+          this.setState({ display: '2017' });
+          setTimeout(() => {
+            this.setState({ display: '2016' });
+            setTimeout(() => {
+              this.setState({ display: '2015' });
+              setTimeout(() => {
+                this.setState({ display: '2014' });
+                setTimeout(() => {
+                  this.setState({ display: '2013' });
+                  setTimeout(() => {
+                    this.setState({ display: '2012' });
+                    setTimeout(() => {
+                      this.setState({ display: '2011' });
+                      this.timeTravel();
+                      setTimeout(() => {
+                        this.setState({ display: 'YEAH' });
+                        setTimeout(() => {
+                          this.setState({ display: 'FUN' });
+                          setTimeout(() => {
+                            this.setState({ display: '#####' });
+                          }, 4000);
+                        }, 3000);
+                      }, 20000);
+                    }, 10);
+                  }, 30);
+                }, 70);
+              }, 120);
+            }, 200);
+          }, 300);
+        }, 500);
+      }, 800);
+    } else {
+      this.setState({ display: 'ERROR' });
+      setTimeout(() => {
+        this.setState({ display: '#####' });
+      }, 750);
+    }
+  }
+
+  timeTravel() {
+    let audio = new Audio(tmSound);
+    audio.play();
   }
 
   render() {
@@ -69,13 +111,7 @@ export default class Box extends Component {
 
     return (
       <div className="Box-cont">
-        <div className="Box-display">
-          {this.state.one}
-          {this.state.two}
-          {this.state.three}
-          {this.state.four}
-          {this.state.five}
-        </div>
+        <div className="Box-display">{this.state.display}</div>
         <div className="Box-dialpad-cont">
           <div className="Box-dialpad">{dialpad}</div>
         </div>
