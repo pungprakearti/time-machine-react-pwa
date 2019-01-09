30ce4cb4faf459c4ffc0e12dd1dcfbed29018e50
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2019-01-08 17:35:59 -0800

    Initial commit.

--- src/App.js
+++ src/App.js
@@ -26,3 +26,13 @@ class App extends Component {
 }
 
 export default App;
+
+/*
+Hip-Hop Star Nate Dogg dies on March 16th.
+The Royal Wedding between Kate Middleton and Prince William takes place in the United Kingdom on April 29th .
+Poet Gil Scott-Heron dies on May 28th.
+Singer Amy Winehouse dies on July 23rd.
+The popular band R.E.M. announces they are breaking up after thirty-one years.
+Comedian Russell Brand and Singer Katy Perry file for divorce at the end of the year.
+US Actor Charlie Sheen is fired from one of the most popular TV comedies Two and a Half Men after getting into a dispute with the show's creator and demonstrating increasingly erratic behavior.
+*/
