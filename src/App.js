156d4ee5037680883efc9a847719ec54df22d70a
Andrew Pungprakearti <andrew.pungprakearti@gmail.com>
2019-01-09 21:48:39 -0800

    Made buttons animate on press.

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 import TimeMachine from './TimeMachine';
-// import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
@@ -14,13 +13,3 @@ class App extends Component {
 }
 
 export default App;
-
-/*
-Hip-Hop Star Nate Dogg dies on March 16th.
-The Royal Wedding between Kate Middleton and Prince William takes place in the United Kingdom on April 29th .
-Poet Gil Scott-Heron dies on May 28th.
-Singer Amy Winehouse dies on July 23rd.
-The popular band R.E.M. announces they are breaking up after thirty-one years.
-Comedian Russell Brand and Singer Katy Perry file for divorce at the end of the year.
-US Actor Charlie Sheen is fired from one of the most popular TV comedies Two and a Half Men after getting into a dispute with the show's creator and demonstrating increasingly erratic behavior.
-*/
--- src/Box.js
+++ src/Box.js
@@ -1,6 +1,7 @@
 import React, { Component } from 'react';
 import './Box.css';
 import tmSound from './time-machine.mp3';
+import clickSound from './click.wav';
 
 export default class Box extends Component {
   constructor(props) {
@@ -9,23 +10,40 @@ export default class Box extends Component {
       display: '#####'
     };
     this.handleClick = this.handleClick.bind(this);
+    this.audioClick = new Audio(clickSound);
   }
 
   handleClick(evt) {
+    //get dialpad number
     let num = evt.target.textContent;
 
+    let btn = evt.target;
+    evt.target.className = 'Box-dialpad-btn Box-press';
+
+    this.audioClick.play();
+
+    setTimeout(() => {
+      btn.className = 'Box-dialpad-btn';
+    }, 100);
+
+    //loop through display string and search for #
     for (let i = 0; i < this.state.display.length; i++) {
       if (this.state.display[i] === '#') {
+        //
+        //replace current string with previous entry and current number
         let str =
           this.state.display.slice(0, i) +
           num +
           this.state.display.slice(i + 1);
 
+        //on last number, check code
         if (i === 4) {
           this.setState(st => {
             st.display = str;
             this.checkCode(st.display);
           });
+
+          //if not last number, add to state and update display
         } else {
           return this.setState({ display: str });
         }
@@ -33,6 +51,7 @@ export default class Box extends Component {
     }
   }
 
+  //check code and if is correct, time travel
   checkCode(code) {
     if (code === '42069') {
       this.setState({ display: '2019' });
@@ -60,7 +79,7 @@ export default class Box extends Component {
                           setTimeout(() => {
                             this.setState({ display: '#####' });
                           }, 4000);
-                        }, 3000);
+                        }, 2800);
                       }, 20000);
                     }, 10);
                   }, 30);
@@ -78,9 +97,10 @@ export default class Box extends Component {
     }
   }
 
+  //travel through time!
   timeTravel() {
-    let audio = new Audio(tmSound);
-    audio.play();
+    let audioTT = new Audio(tmSound);
+    audioTT.play();
   }
 
   render() {
@@ -90,23 +110,66 @@ export default class Box extends Component {
     let d = 0;
     let dialNums = [1, 2, 3, 4, 5, 6, 7, 8, 9, '', 0, ''];
     let row = [];
+    for (let btn of dialNums) {
+      if (btn === '') {
+        dialpad.push(
+          <div id="Box-dialpad-empty" key={btn}>
+            {btn}
+          </div>
+        );
+      } else {
+        dialpad.push(
+          <div
+            className="Box-dialpad-btn"
+            key={btn}
+            onClick={this.handleClick}
+            style={{ zIndex: btn > 0 ? btn : 10 }}
+          >
+            {btn}
+          </div>
+        );
+      }
+    }
+    /*
     for (let i = 0; i < 4; i++) {
       let col = [];
       for (let j = 0; j < 3; j++) {
         if (dialNums[d] === '') {
-          col.push(<td id="Box-dialpad-empty">{dialNums[d]}</td>);
+          col
+            .push
+            // <td id="Box-dialpad-empty" key={d}>
+            //   {dialNums[d]}
+            // </td>
+            // <div id="Box-dialpad-empty" key={d}>
+            //   {dialNums[d]}
+            // </div>
+            ();
         } else {
-          col.push(<td onClick={this.handleClick}>{dialNums[d]}</td>);
+          col
+            .push
+            // <td key={d} onClick={this.handleClick}>
+            //   {dialNums[d]}
+            // </td>
+            // <div key={d} onClick={this.handleClick}>
+            //   {dialNums[d]}
+            // </div>
+            ();
         }
         d++;
       }
-      row.push(<tr>{col}</tr>);
+      // row.push(<tr>{col}</tr>);
+      // row.push({ col });
+      // console.log(row);
     }
-    dialpad.push(
-      <table>
-        <tbody>{row}</tbody>
-      </table>
-    );
+    dialpad
+      .push
+      // <table>
+      //   <tbody>{row}</tbody>
+      // </table>
+      // <div>{row}</div>
+      ();
+    // console.log(dialpad);
+    */
 
     return (
       <div className="Box-cont">
